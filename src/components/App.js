bebaec335d22290349284ecf0f0c85f2bab9c12b
2NTH <hieuntctk42@gmail.com>
2020-07-26 21:32:14 +0700

    Add reducer for spin loader

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 
 import MusicPlayer from './music-player/MusicPlayer';
 import SearchBar from './search-bar/SearchBar';
+import SpinLoader from './spin-loader';
 
 const App = () => {
   return (
     <div className="container">
       <SearchBar />
       <MusicPlayer />
+
+      <SpinLoader />
     </div>
   );
 };
