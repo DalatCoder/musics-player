af65ef38ece479b55b93f983a2b99069bfea76aa
2NTH <hieuntctk42@gmail.com>
2020-07-26 21:10:07 +0700

    Add reducer for fetch status

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import playStatusReducer from './playStatusReducer';
 import songReducer from './songReducer';
 import songsReducer from './songsReducer';
+import fetchSongsStatusReducer from './fetchSongStatusReducer';
 import progressReducer from './progressReducer';
 import suggestBoxReducer from './suggestBoxReducer';
 
@@ -9,6 +10,7 @@ export default combineReducers({
   isPlaying: playStatusReducer,
   currentSong: songReducer,
   progressPercent: progressReducer,
+  showSuggest: suggestBoxReducer,
   fetchedSongs: songsReducer,
-  showSuggest: suggestBoxReducer
+  canFetchSong: fetchSongsStatusReducer
 });
