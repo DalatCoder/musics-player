686665c797f18a19181752177c6823c8c46c5449
2NTH <hieuntctk42@gmail.com>
2020-08-04 09:29:23 +0700

    Add Suggest component

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import './Aside.css';
-import { getImageURL, formatDuration } from '../utils';
-
-const Song = ({ song, order }) => {
-  const songOrder = order ? <div className="song-order">{order}</div> : null;
-
-  return (
-    <div className="song">
-      {songOrder}
-      <div className="song-photo">
-        <img src={getImageURL(song.thumb)} alt={`Thumbnail of ${song.name}`} />
-      </div>
-      <div className="song-info">
-        <div className="title">{song.name}</div>
-        <div className="artist">{song.artist}</div>
-      </div>
-      <div className="song-duration">{formatDuration(song.duration)}</div>
-    </div>
-  );
-};
+import Song from './Song';
 
 const Aside = ({ songs }) => {
   return (
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import './SearchBar.css';
+import Suggest from './Suggest';
+import { songs } from './testData';
 
-const SearchBar = () => {
-  return (
-    <div className="search-bar col-xl-10 col-lg-12">
-      <div className="search-container">
-        <form className="form" id="form">
-          <input
-            type="text"
-            id="search"
-            autoComplete="off"
-            placeholder="Nhập tên bài hát"
-            autoCorrect="off"
-            autoCapitalize="off"
-            spellCheck="false"
-          />
-          <button type="submit" className="btn">
-            <i className="fas fa-search" />
-          </button>
-        </form>
-        <div id="suggest" className="suggest" />
+class SearchBar extends React.Component {
+  state = {
+    showSuggest: true,
+    songs: Object.values(songs)
+  };
+
+  render() {
+    return (
+      <div className="search-bar col-xl-10 col-lg-12">
+        <div
+          className={`search-container ${
+            this.state.showSuggest ? 'show-suggest' : ''
+          }`}
+        >
+          <form className="form">
+            <input
+              type="text"
+              id="search"
+              autoComplete="off"
+              placeholder="Nhập tên bài hát"
+              autoCorrect="off"
+              autoCapitalize="off"
+              spellCheck="false"
+            />
+            <button type="submit" className="btn">
+              <i className="fas fa-search" />
+            </button>
+          </form>
+          {this.state.showSuggest ? (
+            <Suggest songs={this.state.songs.slice(0, 5)} />
+          ) : null}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SearchBar;
